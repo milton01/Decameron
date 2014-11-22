f0ea0dae424a9fbf5eacaf223874a545e6e841ab
Stanley Arevalo <stanley.rear@gmail.com>
2014-11-22 13:13:51 -0600

    cambios

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'Mazda62005';
+$db['default']['password'] = '';
 $db['default']['database'] = 'decameron';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'decameron_';
