06e18b36d1019618c1eac7ca88384f339bc4cc03
Stanley Arevalo <stanley.rear@gmail.com>
2014-11-22 13:38:26 -0600

    Cambios en header y footer

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '54.148.151.242';
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
 $db['default']['database'] = 'decameron';
--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -37,7 +37,6 @@ html {
 	<div id="menubar_container">
 		<div id="menubar_company_info">
 		<span id="company_title"><?php echo $this->config->item('company'); ?></span><br />
-		<span style='font-size:8pt;'><?php echo $this->lang->line('common_powered_by').' Open Source Point Of Sale'; ?></span>
 	</div>
 
 		<div id="menubar_navigation">
